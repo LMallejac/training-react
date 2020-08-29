483c5b093fac3b8257637b2c07ee32c21480cc63
Lucas Malléjac <mallejac.lucas@gmail.com>
2020-08-29 19:57:00 +0100

    Testing of props and components

--- src/App.js
+++ src/App.js
@@ -3,9 +3,30 @@ import "./styles.css";
 
 export default function App() {
   return (
+    <AppWrapper>
+      <DeconstructionOne name='CodeSandbox' surname='Git' />
+      <DeconstructionTwo text='random text lucas' color='blue' />
+    </AppWrapper>
+  );
+}
+
+function AppWrapper ({children}) {
+  return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      {children}
     </div>
   );
+}
+
+function DeconstructionOne ({name, surname}) {
+  return <h1>Hello {name} {surname} </h1>;
+}
+
+function DeconstructionTwo (props) {
+
+  const { text, color } = props;
+
+  console.log(color);
+  const css = `color: ${color};`;
+  return <h2>{text}</h2>;
 }
