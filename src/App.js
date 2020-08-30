06e1a5b4fe92c09f854c32d29d81e3cc0366c867
Lucas Malléjac <mallejac.lucas@gmail.com>
2020-08-30 18:00:47 +0100

    Added buttons and tried anonymous functions

--- src/App.js
+++ src/App.js
@@ -1,32 +1,60 @@
-import React from "react";
-import "./styles.css";
+import React from 'react';
+import './styles.css';
+
+import { Button } from 'sancho';
 
 export default function App() {
   return (
-    <AppWrapper>
-      <DeconstructionOne name='CodeSandbox' surname='Git' />
-      <DeconstructionTwo text='random text lucas' color='blue' />
-    </AppWrapper>
+    <React.Fragment>
+      <AppWrapper>
+        <DeconstructionOne name="CodeSandbox" surname="Git" />
+        <DeconstructionTwo text="random text lucas" color="blue" />
+      </AppWrapper>
+      <Buttons />
+    </React.Fragment>
   );
 }
 
-function AppWrapper ({children}) {
+function AppWrapper({ children }) {
+  return <div className="App">{children}</div>;
+}
+
+function DeconstructionOne({ name, surname }) {
   return (
-    <div className="App">
-      {children}
-    </div>
+    <h1>
+      Hello {name} {surname}{' '}
+    </h1>
   );
 }
 
-function DeconstructionOne ({name, surname}) {
-  return <h1>Hello {name} {surname} </h1>;
-}
-
-function DeconstructionTwo (props) {
-
+function DeconstructionTwo(props) {
   const { text, color } = props;
 
   console.log(color);
   const css = `color: ${color};`;
   return <h2>{text}</h2>;
+}
+
+function Buttons() {
+  const functionOnClickHTMLButton1 = function () {
+    console.log('clicked on the html button!');
+  };
+
+  const functionOnClickHTMLButton2 = () => {
+    console.log('clicked on the html button!');
+  };
+
+  const buildOnClickFunction = (buttonType) => {
+    return () => {
+      console.log(buttonType);
+    };
+  };
+
+  return (
+    <React.Fragment>
+      <button onClick={functionOnClickHTMLButton2}>NOT Sancho Button !</button>
+      <Button>Sancho Button1 !</Button>
+      <Button onClick={buildOnClickFunction('Sancho')}>Sancho Button2 !</Button>
+    </React.Fragment>
+  );
 }
